822716c485f1ba746a19d3d46a7f3550201a2600
Temitope David <64348428+TEECOD3@users.noreply.github.com>
2022-08-17 13:11:54 +0100

    making click events work on the three btns , also on they overlay. through this i learnt tto add and remove class from an element selected and i also learnt to make use of loop.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const showModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modalCard = document.querySelector('.modal');
+
+const openModal = function () {
+  overlay.classList.remove('hidden');
+  modalCard.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modalCard.classList.add('hidden');
+};
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
